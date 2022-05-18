20d8440575a3bd60c7929edf0be501d448991578
Patrick Akpala <patrick.akpala@lloydsbanking.com>
2022-05-18 08:48:15 +0100

    Demo: Using let andd const with Typescript
    Learned about let and const variables and how
    typescript enforces their correct usage.

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,13 @@
 function startGame() {
-  // starting a new game
+  let playerName: string = 'Audrey';
+  logPlayer(playerName);
+
   var messagesElement = document.getElementById('messages');
   messagesElement!.innerText = 'Welcome to MultiMath! Staring new game...';
+}
+
+function logPlayer(name) {
+  console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
